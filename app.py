e04881be9d59a6544aa5f1f928af32e4f973bf5f
Piyush Solanki <piyushsolanki18dec@gmail.com>
2025-05-24 11:40:38 +0530

    openssl installation

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'secure-video-generator'
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(ssl_context=('certs/cert.pem', 'certs/key.pem'))
